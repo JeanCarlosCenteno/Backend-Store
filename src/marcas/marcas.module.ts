12219faf54b799df3b7e5c445cd23ef29642da4f
JeanCarlosCenteno <CentenoJean13@gmail.com>
2023-08-18 21:26:31 -0600

    creando crud y agregando funcionlidad

---
+++ src/marcas/marcas.module.ts
@@ -0,0 +1,16 @@
+import { Module } from "@nestjs/common/decorators/modules/module.decorator";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Marca } from "./entities/marca.entity";
+import { MarcasService } from "./services/marcas.service";
+import { MarcasController } from './controller/marcas.controller';
+
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Marca])],
+    providers: [MarcasService],
+    //aqui los servicios
+    controllers:[MarcasController],
+    //aqui los controladores
+    
+})
+export class MarcasModule{}
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
+import { ProductsController } from './controllers/products.controller';
+import { ProductService } from './services/products.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product])],
-  controllers: [],
-  providers: [],
+  controllers: [ProductsController],
+  providers: [ProductService],
 })
 export class ProductsModule {}
