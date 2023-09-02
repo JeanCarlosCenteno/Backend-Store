0de389f8bd52296df7156518410e2debb719cabe
JeanCarlosCenteno <CentenoJean13@gmail.com>
2023-09-02 15:13:50 -0600

    Creando funcionalidad para File agregando Imag

--- src/files/controllers/files.controller.ts
+++ src/files/controllers/files.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Post, Get, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { Controller, Post, UseInterceptors, UploadedFile,BadRequestException } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { fileFilter } from 'src/helpers/fileFilter.helper';
 import { FilesService } from '../services/files.service';
+import { fileNamer } from '../../helpers/fileNamer.helper';
 
 @Controller('files')
 export class FilesController {
@@ -8,14 +11,22 @@ export class FilesController {
         private readonly filesService: FilesService) {}
 
     @Post('upload')
-    @UseInterceptors(FileInterceptor('file'))
-    uploadImage(@UploadedFile() file: Express.Multer.File){
-        console.log(file);
-    }
+    @UseInterceptors(FileInterceptor('file', {
+        //Llamamos al fileFilter de muter y le asignamos nuestro helper
+        fileFilter: fileFilter,
 
-    @Get('product/:imageId')
-    getImage() {
-        return 'Hola';
+        //Definimos el almacenamiento donde se va aguardar y lo renombramos
+        storage: diskStorage({
+            destination: './static/products/',
+            filename: fileNamer,
+        })
+    }))
+    uploadImage(@UploadedFile() file: Express.Multer.File){
+        if(!file) {
+            throw new BadRequestException('Asegurese que el archivo sea una imagen');
+        }
+        return {
+            fileName: file.filename,
+        };
     }
-    
 }
