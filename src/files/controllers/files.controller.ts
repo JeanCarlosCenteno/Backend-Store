f4b62e9f2e8e3f8263f67c89e75cfd844c61a0df
JeanCarlosCenteno <CentenoJean13@gmail.com>
2023-09-30 10:08:30 -0600

    Modificando el archivito files y creado rutas para agregar imagenes

--- src/files/controllers/files.controller.ts
+++ src/files/controllers/files.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Post, UseInterceptors, UploadedFile,BadRequestException } from '@nestjs/common';
+import { Controller, Post, Get, Res, Param, UseInterceptors, UploadedFile,BadRequestException }
+from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { fileFilter } from 'src/helpers/fileFilter.helper';
 import { FilesService } from '../services/files.service';
 import { fileNamer } from '../../helpers/fileNamer.helper';
+import { Response } from 'express';
 
 @Controller('files')
 export class FilesController {
@@ -25,8 +27,15 @@ export class FilesController {
         if(!file) {
             throw new BadRequestException('Asegurese que el archivo sea una imagen');
         }
-        return {
-            fileName: file.filename,
-        };
+        const  url  = `${file.filename}`;
+
+        return { url };
+    }
+    @Get('product/:imageName')
+    findProduct(@Res() res:Response, @Param('imageName') imageName: string) {
+        const path = this.filesService.getStaticImageName(imageName);
+
+        // return path;
+        res.sendFile(path);
     }
 }
--- src/files/services/files.service.ts
+++ src/files/services/files.service.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { existsSync } from 'fs';
+import { join } from 'path';
 
 @Injectable()
 export class FilesService {
-    constructor() {
+    constructor() {}
 
+    getStaticImageName(imageName: string) {
+    // Agregando la ruta para encontrar el archivo
+        const path = join(__dirname, '../../../static/products', imageName);
+
+        if (!existsSync(path)) {
+            throw new BadRequestException(
+            `No existe un producto con la imagen ${imageName}`,
+            );
+        }
+        // Si existe la imagen entonces que la retorne
+        return path;
     }
 }
